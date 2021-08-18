1f4d4db511d5ac25eb6c50a81278890e341bc8d6
KimWonJun <kwj0313im@naver.com>
2021-08-18 23:15:29 +0900

    초기 환경설정 및 테스트

---
+++ DevProject/src/main/java/com/dev/controller/HomeController.java
@@ -0,0 +1,37 @@
+package com.dev.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController 
+{
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@GetMapping("/")
+	public String home(Locale locale, Model model)
+	{
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		LocalDateTime now = LocalDateTime.now();
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 (E) a h시 m분 s초");
+		String formattedNow = now .format(formatter);
+		
+		model.addAttribute("serverTime", formattedNow);
+		
+		return "home";
+	}
+	
+	
+}
